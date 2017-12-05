3787b0502a85ec630c52eb409403c8a9ce6432bf
Sean Matheson <sean@ctrlplusb.com>
2017-12-05 18:57:54 +0000

    Fixes global styles.

--- src/containers/Theme/index.js
+++ src/containers/Theme/index.js
@@ -1,12 +1,10 @@
 import "glamor/reset";
+import "./globals.css";
 import React from "react";
 import { ThemeProvider } from "glamorous";
-import { css } from "glamor";
 import * as theme from "./theme";
 
-import globalStyles from "./globals";
-
 export default function Theme({ children }) {
-  css.insert(globalStyles);
+  // css.insert(globalStyles);
   return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
